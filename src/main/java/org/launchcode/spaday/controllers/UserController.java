54b3c7578ce9b50427751e3b10ba2cc517016ae9
Tiah <tiahmack@yahoo.com>
2022-05-23 20:46:33 -0500

    studio part 2

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,33 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @GetMapping(value = "/add")
+        public String displayAddUserForm(){
+        return "user/add";
+}
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        if (user.getPassword().equals(verify)){
+            return "user/index";
+        } else{
+            model.addAttribute("error", "Password does not match");
+            return "user/add";
+        }
+    }
+
+
+}
---
+++ src/main/java/org/launchcode/spaday/models/Client.java
@@ -0,0 +1,4 @@
+package org.launchcode.spaday.models;
+
+public class Client {
+}
